5408d04b8b88fa311a24787581e57be4df3662a0
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-10-04 14:00:36 -0300

    Created Maven empty project.

---
+++ src/main/java/me/brunosantana/java8_date_time_api/App.java
@@ -0,0 +1,13 @@
+package me.brunosantana.java8_date_time_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
