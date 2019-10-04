02334c3994b7db5a012575e457e628b1b1919630
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-10-04 15:38:11 -0300

    Add examples using timezone

---
+++ src/main/java/me/brunosantana/java8_date_time_api/example4/NewDateTimeApiExample4.java
@@ -0,0 +1,41 @@
+package me.brunosantana.java8_date_time_api.example4;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class NewDateTimeApiExample4 {
+
+	public static void main(String[] args) {
+		
+		convertFromZonedDateTimeToOtherTypes();
+		
+		convertFromOneTimezoneToAnother();
+		
+	}
+
+	private static void convertFromZonedDateTimeToOtherTypes() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("America/Sao_Paulo"));
+		LocalDate localDate = zonedDateTime.toLocalDate();
+		LocalTime localTime = zonedDateTime.toLocalTime();
+		LocalDateTime localDateTime2 = zonedDateTime.toLocalDateTime();
+		
+		System.out.println(localDate);
+		System.out.println(localTime);
+		System.out.println(localDateTime2);
+	}
+
+	private static void convertFromOneTimezoneToAnother() {
+		LocalDateTime dateTime = LocalDateTime.now();
+		
+		ZonedDateTime saoPauloTimezone = dateTime.atZone(ZoneId.of("America/Sao_Paulo"));
+		System.out.println(saoPauloTimezone);
+		
+		ZonedDateTime seoulTimezone = saoPauloTimezone.withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+		System.out.println(seoulTimezone);
+	}
+
+}
